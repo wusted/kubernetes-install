90362557d9798dce32f07fe5c5df5bf95a9d3c39
Jean Pereira <jeankpayales444@gmail.com>
2022-12-06 00:54:30 -0600

    Automates Deployment of 1 VM in KVM Libvirt, use for testing

---
+++ 2.automate_kvm_guest_deployment.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+## Define variables
+# Memory in MiB
+echo -n "VM Memory Size in MiB: (Default 2048) "
+read MEMORY
+MEM_SIZE=$MEMORY
+# Pending, work in conditional for default
+
+# CPU Cores Count
+echo -n "vCPU Cores Count: (Default 2) "
+read CPU
+VCPUS=$CPU
+# Pending, work in conditional for default
+
+# OSVariant - osinfo-query os
+echo -n "Set OS Variant, List of option: # osinfo-query os, default rhel9.1 "
+read OS
+OS_VARIANT=$OS
+# Pending, work in conditional for default
+
+# ISO File Location
+echo -en "Set the Location for the ISO File for Installation: "
+read ISO_FILE
+
+# Disk File Creation Path
+echo -en "Set the Location for the New Disk File to be created: "
+read DISK_PATH
+
+# VM Name
+echo -en "Set VM Name: "
+read VM_NAME
+
+# OS Type
+OS_TYPE="linux"
+
+# Virtual Disk Size
+echo -en "Set Virtual Disk Size: "
+read DISK_SIZE
+
+
+## Deployment Command
+sudo virt-install \
+     --name ${VM_NAME} \
+     --memory ${MEM_SIZE} \
+     --vpcus {$VCPUS} \
+     --os-type{$OS_TYPE} \
+     --location={$ISO_FILE} \
+     --disk path={$DISK_PATH} \
+     --disk size={$DISK_SIZE} \
+     --network bridge=virbr0 \
+     --nographics \
+     --os-variant={$OS_VARIANT} \
+     --initrd-inject ks.cfg \
+     # Local ks.cfg file
+     --extra-args "inst.ks=file:/ks.cfg console=ttyS0" 
+     # Remote ks.cfg
+     #--extra-args "ks=http/nfs://IP/ks.cfg console=ttyS0"
+
+
+#--disk path=/home/kvm/vdisks/k8s-master1,size=85 
+# --extra-args "console=tty0 console=ttyS0,115200n8"
