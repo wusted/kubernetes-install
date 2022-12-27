a043ca169b3ad87c51cffd7e2230735adee3303c
Jean Pereira <jeankpayales444@gmail.com>
2022-12-27 02:01:29 -0600

    adding circle-ci provider action output

--- 13-cicd-terraform-circleci-kube-digitalocean/02-output-kubeconfig-circleci.tf
+++ 13-cicd-terraform-circleci-kube-digitalocean/02-output-kubeconfig-circleci.tf
@@ -3,9 +3,9 @@ resource "local_file" "kubernetes_config" {
     filename = "kubeconfig.yaml"
 }
 
-# resource "circleci_environment_variable" "kubernetes_config" {
-#     project = "circle-ci-test"
-#     name = "KUBERNETES_KUBECONFIG"
-#     value = "${base64encode(digitalocean_kubernetes_cluster.jean.kube_config.0.raw_config)}"
+resource "circleci_environment_variable" "kubernetes_config" {
+    project = "circle-ci-test"
+    name = "KUBERNETES_KUBECONFIG"
+    value = "${base64encode(digitalocean_kubernetes_cluster.jean.kube_config.0.raw_config)}"
 
-# }
+}
