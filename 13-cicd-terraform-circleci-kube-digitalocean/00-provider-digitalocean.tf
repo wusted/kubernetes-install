c1311b210397815cd936c00f0bb01049dd6a0dd5
Jean Pereira <jeankpayales444@gmail.com>
2022-12-27 01:55:05 -0600

    fixing providers

--- 13-cicd-terraform-circleci-kube-digitalocean/00-provider-digitalocean.tf
+++ 13-cicd-terraform-circleci-kube-digitalocean/00-provider-digitalocean.tf
@@ -1,13 +1,18 @@
 variable "do_token" {}
-variable "circleci_token" {}
+# variable "circleci_token" {}
+
 
 terraform {
-    required_providers {
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "~> 2.0"
-        }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.25.2"
     }
+    circleci = {
+      source = "mrolla/circleci"
+      version = "0.6.1"
+    }
+  }
 }
 
 # Configure the DigitalOcean Provider
@@ -17,7 +22,6 @@ provider "digitalocean" {
 
 # Configure the CircleCI Token
 provider "circleci" {
-    api_token = "${var.circleci_token}"
-    vcs_type = "github"
-    organization = "wusted"
-}
+  api_token = "${var.circleci_token}"
+  vcs_type = "github"
+  organization = "wusted"
