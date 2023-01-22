624289276ff1f3558b5c0fe077fb6605d115b07a
Jean Pereira <jeankpayales444@gmail.com>
2023-01-22 17:22:45 -0600

    Thanos Metric Storage for Prometheus Multi-Cluster

--- 23-thanos-metrics/01-kubernetes-digitalocean.tf
+++ 23-thanos-metrics/01-kubernetes-digitalocean.tf
@@ -1,13 +1,25 @@
-resource "digitalocean_kubernetes_cluster" "jean" {
-    name = "jean"
+resource "digitalocean_kubernetes_cluster" "jean-1" {
+    name = "jean-1"
     region = "nyc1"
     version = "1.25.4-do.0"
 
     node_pool {
-      name = "jean-nodes"
+      name = "jean-1-nodes"
       size = "s-1vcpu-2gb"
       node_count = 2
-      tags = ["jean-nodes"]
+      tags = ["jean-1-nodes"]
     }
 }
 
+resource "digitalocean_kubernetes_cluster" "jean-2" {
+  name = "jean-2"
+  region = "nyc1"
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name = "jean-2-nodes"
+    size = "s-1vcpu-2gb"
+    node_count = 2
+    tags = ["jean-2-nodes"]
+  }
+}
--- 23-thanos-metrics/02-output-digitalocean-kubeconfig.tf
+++ 23-thanos-metrics/02-output-digitalocean-kubeconfig.tf
@@ -1,4 +1,9 @@
-resource "local_file" "kubernetes_config" {
-    content = "${digitalocean_kubernetes_cluster.jean.kube_config.0.raw_config}"
-    filename = "do_kubeconfig.yaml"
+resource "local_file" "kubernetes-1_config" {
+    content = "${digitalocean_kubernetes_cluster.jean-1.kube_config.0.raw_config}"
+    filename = "jean-1_kubeconfig.yaml"
+}
+
+resource "local_file" "kubernetes-2_config" {
+    content = "${digitalocean_kubernetes_cluster.jean-2.kube_config.0.raw_config}"
+    filename = "jean-2_kubernetes.yaml"
 }
