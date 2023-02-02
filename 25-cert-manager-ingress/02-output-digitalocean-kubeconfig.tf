028227187079e66a382a2ea48a3bfdf6ef9243ce
Jean Pereira <jeankpayales444@gmail.com>
2023-02-02 00:16:25 -0600

    Cert-Manager for Nginx Ingress

--- 25-cert-manager-ingress/02-output-digitalocean-kubeconfig.tf
+++ 25-cert-manager-ingress/02-output-digitalocean-kubeconfig.tf
@@ -1,4 +1,4 @@
 resource "local_file" "kubernetes_config" {
-    content = "${digitalocean_kubernetes_cluster.jean-nginx-cert-manager.kube_config.0.raw_config}"
+    content = "${digitalocean_kubernetes_cluster.jean.kube_config.0.raw_config}"
     filename = "kubeconfig.yaml"
 }
--- 25-cert-manager-ingress/03-dns.tf
+++ 25-cert-manager-ingress/03-dns.tf
@@ -7,7 +7,7 @@ resource "digitalocean_domain" "pereirajean" {
 ## Add A record to the Domain
 
 resource "digitalocean_record" "hello" {
-    domain = "${digitalocean_domain.pereirajean.com.name}"
+    domain = "${digitalocean_domain.pereirajean.name}"
     type = "A"
     name = "hello"
     value = "1.1.1.1" # Replace in UI with IP from Kubernetes Created Load Balancer.
